14ae2a0cba3ff6c395d0664d67605b753536c26e
QuintonEL <quintonlaborde@gmail.com>
2020-01-21 20:42:58 +0000

    added functionality to delete buttons to remove short url from page and redirect to /url page

--- express_server.js
+++ express_server.js
@@ -62,3 +62,10 @@ app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
+
+app.post("/urls/:shortURL/delete", (req, res) => {
+  let shorty = req.params.shortURL;
+  delete urlDatabase[shorty];
+  //console.log(urlDatabase)
+  res.redirect("/urls");
+});
